cce72b06ee273f2b29c1e6d4c6e788bfbaf2e3e2
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-03-07 14:30:16 +0100

    remove AMD/commonjs support

--- src/howler.core.js
+++ src/howler.core.js
@@ -1625,32 +1625,8 @@
     Howler._setup();
   };
 
-  // Add support for AMD (Asynchronous Module Definition) libraries such as require.js.
-  if (typeof define === 'function' && define.amd) {
-    define([], function() {
-      return {
-        Howler: Howler,
-        Howl: Howl
-      };
-    });
-  }
-
-  // Add support for CommonJS libraries such as browserify.
-  if (typeof exports !== 'undefined') {
-    exports.Howler = Howler;
-    exports.Howl = Howl;
-  }
-
-  // Define globally in case AMD is not available or unused.
-  if (typeof window !== 'undefined') {
-    window.HowlerGlobal = HowlerGlobal;
-    window.Howler = Howler;
-    window.Howl = Howl;
-    window.Sound = Sound;
-  } else if (typeof global !== 'undefined') { // Add to global in Node.js (for testing, etc).
-    global.HowlerGlobal = HowlerGlobal;
-    global.Howler = Howler;
-    global.Howl = Howl;
-    global.Sound = Sound;
-  }
+  window.HowlerGlobal = HowlerGlobal;
+  window.Howler = Howler;
+  window.Howl = Howl;
+  window.Sound = Sound;
 })();
